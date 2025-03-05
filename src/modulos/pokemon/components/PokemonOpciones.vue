0d2e695545286273cadcd63941328ad7d412d5d8
aramval <annaramosvalldeperez@gmail.com>
2025-03-05 20:32:09 +0100

    Pantalla del juego

---
+++ src/modulos/pokemon/components/PokemonOpciones.vue
@@ -0,0 +1,18 @@
+<template>
+    <ul>
+        <li class="bg-white shadow-md rounded-lg p-3 my-2 cursor-pointer 2-40 text-center hover:bg-gray-100 transition-all">Pokèmon 1</li>
+        <li class="bg-white shadow-md rounded-lg p-3 my-2 cursor-pointer 2-40 text-center hover:bg-gray-100 transition-all">Pokèmon 2</li>
+        <li class="bg-white shadow-md rounded-lg p-3 my-2 cursor-pointer 2-40 text-center hover:bg-gray-100 transition-all">Pokèmon 3</li>
+        <li class="bg-white shadow-md rounded-lg p-3 my-2 cursor-pointer 2-40 text-center hover:bg-gray-100 transition-all">Pokèmon 4</li>
+    </ul>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+@import "tailwindcss";
+
+ /*li {
+    @apply bg-white shadow-md rounded-lg p-3 my-2 cursor-pointer 2-40 text-center hover:bg-gray-100 transition-all*
+ }*/
+</style>
---
+++ src/modulos/pokemon/paginas/PokemonLudum.vue
@@ -0,0 +1,22 @@
+<template>
+
+    <section v-if="false" class="flex flex-col justify-content items-center w-screen h-screen">
+        <h1 class="text-3xl">Espere, por favor</h1>
+        <h3 class="animate-pulse">Cargando Pokèmon</h3>
+    </section>
+
+    <section class="flex flex-col justify-center items-center w-screen h-screen">
+        <h1 class="m-5">¿Quién es este Pokèmon?</h1>
+
+        <PokemonImago/>
+        <PokemonOpciones/>
+
+    </section>
+</template>
+
+<script setup lang="ts">
+import PokemonImago from '../components/PokemonImago.vue';
+import PokemonOpciones from '../components/PokemonOpciones.vue';
+
+
+</script>
